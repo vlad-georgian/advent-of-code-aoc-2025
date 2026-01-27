17468e1ff010fc2940c39c0c7a26ca6029b8284b
vlad-georgian <144853024+vlad-georgian@users.noreply.github.com>
2026-01-27 04:44:53 +0200

    Day 4 code cleanup + comments - Added signing

--- day-4/index.ts
+++ day-4/index.ts
@@ -1,104 +1,115 @@
+const adjacentRollLimit = 4;
+
 function getRollsAroundItem(diagram: string[][], i: number, j: number): number {
     let rollCount = 0;
-    const rowCount = diagram.length
-    const columnCount = diagram[ 0 ].length
+    const rowCount = diagram.length;
+    const columnCount = diagram[0].length;
 
-    const check = (item: string): boolean => [ '@', "x" ].includes(item)
+    const isRollOfPaper = (item: string): boolean => ['@', 'x'].includes(item);
 
+    // Top row
     if (i > 0) {
-        // top row
-        rollCount += j > 0 && check(diagram[ i - 1 ][ j - 1 ]) ? 1 : 0
-        rollCount += check(diagram[ i - 1 ][ j ]) ? 1 : 0
-        rollCount += j < columnCount - 1 && check(diagram[ i - 1 ][ j + 1 ]) ? 1 : 0
+        rollCount += j > 0 && isRollOfPaper(diagram[i - 1][j - 1]) ? 1 : 0;
+        rollCount += isRollOfPaper(diagram[i - 1][j]) ? 1 : 0;
+        rollCount += j < columnCount - 1 && isRollOfPaper(diagram[i - 1][j + 1]) ? 1 : 0;
     }
 
-    // current row
-    rollCount += j > 0 && check(diagram[ i ][ j - 1 ]) ? 1 : 0
-    rollCount += j < columnCount - 1 && check(diagram[ i ][ j + 1 ]) ? 1 : 0
+    // Current row
+    rollCount += j > 0 && isRollOfPaper(diagram[i][j - 1]) ? 1 : 0;
+    rollCount += j < columnCount - 1 && isRollOfPaper(diagram[i][j + 1]) ? 1 : 0;
 
-    // bottom row
+    // Bottom row
     if (i < rowCount - 1) {
-        rollCount += j > 0 && check(diagram[ i + 1 ][ j - 1 ]) ? 1 : 0
-        rollCount += check(diagram[ i + 1 ][ j ]) ? 1 : 0
-        rollCount += j < columnCount - 1 && check(diagram[ i + 1 ][ j + 1 ]) ? 1 : 0
+        rollCount += j > 0 && isRollOfPaper(diagram[i + 1][j - 1]) ? 1 : 0;
+        rollCount += isRollOfPaper(diagram[i + 1][j]) ? 1 : 0;
+        rollCount += j < columnCount - 1 && isRollOfPaper(diagram[i + 1][j + 1]) ? 1 : 0;
     }
 
-    return rollCount
+    return rollCount;
 }
 
 export function partOne(diagram: string[][]): number {
     if (!diagram?.length) {
-        throw new Error('No diagram provided')
+        throw new Error('No diagram provided');
     }
 
-    const rowCount = diagram.length
-    const columnCount = diagram[ 0 ].length
+    const rowCount = diagram.length;
+    const columnCount = diagram[0].length;
     let accessibleCount = 0;
 
-    for (let i = 0; i < rowCount; i++) {
-        for (let j = 0; j < columnCount; j++) {
-            if (diagram[ i ][ j ] !== '@') {
-                continue
+    for (let rowIndex = 0; rowIndex < rowCount; rowIndex++) {
+        for (let columnIndex = 0; columnIndex < columnCount; columnIndex++) {
+            if (diagram[rowIndex][columnIndex] !== '@') {
+                continue;
             }
 
-            const rollsCount = getRollsAroundItem(diagram, i, j)
+            const rollsCount = getRollsAroundItem(diagram, rowIndex, columnIndex);
 
-            if (rollsCount >= 4) {
-                continue
+            if (rollsCount >= adjacentRollLimit) {
+                continue;
             }
 
-            diagram[ i ][ j ] = 'x'
+            diagram[rowIndex][columnIndex] = 'x';
 
-            accessibleCount++
+            accessibleCount++;
         }
     }
 
-    return accessibleCount
+    return accessibleCount;
 }
 
 export function partTwo(diagram: string[][]): number {
     if (!diagram?.length) {
-        throw new Error('No diagram provided')
+        throw new Error('No diagram provided');
     }
 
-    const rowCount = diagram.length
-    const columnCount = diagram[ 0 ].length
+    const rowCount = diagram.length;
+    const columnCount = diagram[0].length;
     let accessibleCount = 0;
 
-    for (let i = 0; i < rowCount; i++) {
-        for (let j = 0; j < columnCount; j++) {
-            if (diagram[ i ][ j ] === 'x') { diagram[ i ][ j ] = '.' }
-            if (diagram[ i ][ j ] !== '@') {
-                continue
+    for (let rowIndex = 0; rowIndex < rowCount; rowIndex++) {
+        for (let columnIndex = 0; columnIndex < columnCount; columnIndex++) {
+            // Use this instead if you want to visualize each step
+            // if (diagram[rowIndex][columnIndex] === 'x') {
+            //     diagram[rowIndex][columnIndex] = '.';
+            // }
+            if (diagram[rowIndex][columnIndex] !== '@') {
+                continue;
             }
 
-            const rollsCount = getRollsAroundItem(diagram, i, j)
+            const rollsCount = getRollsAroundItem(diagram, rowIndex, columnIndex);
 
-            if (rollsCount >= 4) {
-                continue
+            if (rollsCount >= adjacentRollLimit) {
+                continue;
             }
 
-            diagram[ i ][ j ] = 'x'
+            diagram[rowIndex][columnIndex] = '.';
+            // Use this instead if you want to visualize each step
+            // diagram[rowIndex][columnIndex] = 'x';
 
-            accessibleCount++
+            accessibleCount++;
         }
     }
 
     if (accessibleCount === 0) {
-        return 0
+        return 0;
     }
 
-    const secondRunCount = partTwo(diagram)
+    const secondRunCount = partTwo(diagram);
 
-    return accessibleCount + secondRunCount
+    return accessibleCount + secondRunCount;
 }
 
 export function parse(input: string): string[][] {
-    return input.toString().trim().split('\r\n').map((row: string): string[] => row.split(''))
+    return input
+        .toString()
+        .trimEnd()
+        .split('\r\n')
+        .map((row) => row.split(''));
 }
 
-export function* solve(input: string): Generator<[ string, string, number ]> {
-    yield [ 'Day 4: Printing department - Part 1', 'Max accessible', partOne(parse(input)) ]
+export function* solve(input: string): Generator<[string, string, number]> {
+    yield ['Day 4: Printing department - Part 1', 'Max accessible', partOne(parse(input))];
 
-    return [ 'Day 4: Printing department - Part 2', 'Max accessible', partTwo(parse(input)) ]
+    return ['Day 4: Printing department - Part 2', 'Max accessible', partTwo(parse(input))];
 }
