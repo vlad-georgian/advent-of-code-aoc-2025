c1ea3de9469a8bf381184b25b95e43b4e71e9aad
vlad-georgian <144853024+vlad-georgian@users.noreply.github.com>
2026-01-27 04:32:08 +0200

    Switched to oxc + github actions
    - Removed Biome - Switched to oxc linter and formater - Added gh actions for linting and testing

--- index.ts
+++ index.ts
@@ -1,57 +1,60 @@
-import * as fs from 'node:fs/promises'
-import * as dayOne from './day-1'
-import * as dayTwo from './day-2'
-import * as dayThree from './day-3'
-import * as dayFour from './day-4'
+import * as fs from 'node:fs/promises';
+
+import * as dayOne from './day-1';
+import * as dayTwo from './day-2';
+import * as dayThree from './day-3';
+import * as dayFour from './day-4';
 
 type Solution = {
-    solve: (data: string) => Generator<[ string, string, number ]>
-}
-const solutions: Solution[] = [ dayOne, dayTwo, dayThree, dayFour ]
+    solve: (data: string) => Generator<[string, string, number]>;
+};
+const solutions: Solution[] = [dayOne, dayTwo, dayThree, dayFour];
 
 // Check against sample data
 async function solve(inputFileName: string) {
     if (!inputFileName?.length) {
-        throw new Error('No input file specified!')
+        throw new Error('No input file specified!');
     }
 
-    console.log(`\r\n'${inputFileName}' dataset:\r\n`)
+    console.log(`\r\n'${inputFileName}' dataset:\r\n`);
 
-    performance.mark(inputFileName)
+    performance.mark(inputFileName);
 
-    for (const [ index, solution ] of solutions.entries()) {
-        performance.mark('solution')
+    for (const [index, solution] of solutions.entries()) {
+        performance.mark('solution');
 
-        console.log('file', `./day-${index + 1}/${inputFileName}.txt`)
-        const dataRaw = await fs.readFile(`./day-${index + 1}/${inputFileName}.txt`)
-        const data = dataRaw.toString()
-        const solutions = solution.solve(data)
+        console.log('file', `./day-${index + 1}/${inputFileName}.txt`);
+        const dataRaw = await fs.readFile(`./day-${index + 1}/${inputFileName}.txt`);
+        const data = dataRaw.toString();
+        const solutions = solution.solve(data);
 
         while (true) {
-            performance.mark('solution-start')
+            performance.mark('solution-start');
 
-            const result = solutions.next()
-            const deltaSolution = performance.measure('solution', 'solution-start')
+            const result = solutions.next();
+            const deltaSolution = performance.measure('solution', 'solution-start');
 
-            console.log(`${result.value[ 0 ]} -> ${result.value[ 1 ]}: ${result.value[ 2 ]} in ${deltaSolution.duration}ms`)
+            console.log(`${result.value[0]} -> ${result.value[1]}: ${result.value[2]} in ${deltaSolution.duration}ms`);
 
             if (result.done) {
-                break
+                break;
             }
         }
     }
 
-    const deltaSet = performance.measure('set', inputFileName)
+    const deltaSet = performance.measure('set', inputFileName);
 
-    console.log(`Time(with input processing): ${deltaSet.duration}ms`)
+    console.log(`Time(with input processing): ${deltaSet.duration}ms`);
 }
 
-performance.mark('all')
+performance.mark('all');
 
-await solve('sample')
-console.log(`\r\n${new Array(80).fill('-').join('')}`)
-await solve('input')
+const separator = Array.from({ length: 80 }).fill('-').join('');
 
-const deltaAll = performance.measure('set', 'all')
-console.log(`\r\n${new Array(80).fill('-').join('')}`)
-console.log(`\r\nTime(total): ${deltaAll.duration}ms`)
+await solve('sample');
+console.log(`\r\n${separator}`);
+await solve('input');
+
+const deltaAll = performance.measure('set', 'all');
+console.log(`\r\n${separator}`);
+console.log(`\r\nTime(total): ${deltaAll.duration}ms`);
--- tests/vitest.js
+++ tests/vitest.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
-import { execaSync } from 'execa'
+import { execaSync } from 'execa';
 
-execaSync('node', ['./node_modules/vitest/bin/vitest.js', 'run', ...process.argv.slice(2)], { stdio: 'inherit' })
+execaSync('node', ['./node_modules/vitest/bin/vitest.js', 'run', ...process.argv.slice(2)], {
+    stdio: 'inherit',
+});
