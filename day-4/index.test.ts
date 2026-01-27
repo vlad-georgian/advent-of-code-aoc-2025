7e5ad4ebcfbcc21761d6cc0c38de336bec16e9ee
vlad-georgian <144853024+vlad-georgian@users.noreply.github.com>
2026-01-27 05:04:31 +0200

    Updated the testing workflow typo

--- day-4/index.test.ts
+++ day-4/index.test.ts
@@ -1,49 +1,50 @@
-import * as fs from 'node:fs/promises'
-import { beforeEach, describe, expect, it, test } from 'vitest'
-import * as dayFour from './index'
+import * as fs from 'node:fs/promises';
+import { beforeEach, describe, expect, it, test } from 'vitest';
 
-const pathPrefix = './day-4'
+import * as dayFour from './index';
+
+const pathPrefix = './day-4';
 
 describe('Day 4: Printing department - Sample dataset', async () => {
-    const dataRaw = await fs.readFile(`${pathPrefix}/sample.txt`)
-    const data = dataRaw.toString()
-    let diagram: string[][] = []
+    const dataRaw = await fs.readFile(`${pathPrefix}/sample.txt`);
+    const data = dataRaw.toString();
+    let diagram: string[][] = [];
 
     beforeEach(async () => {
-        diagram = dayFour.parse(data)
-    })
+        diagram = dayFour.parse(data);
+    });
 
     it('should solve part 1', () => {
-        const result = dayFour.partOne(diagram)
+        const result = dayFour.partOne(diagram);
 
-        expect(result).toEqual(13)
-    })
+        expect(result).toEqual(13);
+    });
 
     it('should solve part 2', () => {
-        const result = dayFour.partTwo(diagram)
+        const result = dayFour.partTwo(diagram);
 
-        expect(result).toEqual(43)
-    })
-})
+        expect(result).toEqual(43);
+    });
+});
 
 describe('Day 4: Printing department - Input dataset', async () => {
-    const dataRaw = await fs.readFile(`${pathPrefix}/input.txt`)
-    const data = dataRaw.toString()
-    let diagram: string[][] = []
+    const dataRaw = await fs.readFile(`${pathPrefix}/input.txt`);
+    const data = dataRaw.toString();
+    let diagram: string[][] = [];
 
     beforeEach(async () => {
-        diagram = dayFour.parse(data)
-    })
+        diagram = dayFour.parse(data);
+    });
 
     it('should solve part 1', () => {
-        const result = dayFour.partOne(diagram)
+        const result = dayFour.partOne(diagram);
 
-        expect(result).toEqual(1533)
-    })
+        expect(result).toEqual(1533);
+    });
 
     it('should solve part 2', () => {
-        const result = dayFour.partTwo(diagram)
+        const result = dayFour.partTwo(diagram);
 
-        expect(result).toEqual(9206)
-    })
-})
+        expect(result).toEqual(9206);
+    });
+});
