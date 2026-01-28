819b14d32491c29718ff4a1ba121fd90a37402ee
vlad-georgian <144853024+vlad-georgian@users.noreply.github.com>
2026-01-28 04:29:45 +0200

    Formatted day 1

--- day-1/index.test.ts
+++ day-1/index.test.ts
@@ -1,49 +1,50 @@
-import * as fs from 'node:fs/promises'
-import { beforeEach, describe, expect, it, test } from 'vitest'
-import * as dayOne from './index'
+import * as fs from 'node:fs/promises';
+import { beforeEach, describe, expect, it, test } from 'vitest';
 
-const pathPrefix = './day-1'
+import * as dayOne from './index';
+
+const pathPrefix = './day-1';
 
 describe('Day 1: Secret entrance - Sample dataset', async () => {
-    const dataRaw = await fs.readFile(`${pathPrefix}/sample.txt`)
-    const data = dataRaw.toString()
-    let lines: string[] = []
+    const dataRaw = await fs.readFile(`${pathPrefix}/sample.txt`);
+    const data = dataRaw.toString();
+    let lines: string[] = [];
 
     beforeEach(async () => {
-        lines = dayOne.parse(data)
-    })
+        lines = dayOne.parse(data);
+    });
 
     it('should solve part 1', () => {
-        const result = dayOne.partOne(lines)
+        const result = dayOne.partOne(lines);
 
-        expect(result).toEqual(3)
-    })
+        expect(result).toEqual(3);
+    });
 
     it('should solve part 2', () => {
-        const result = dayOne.partTwo(lines)
+        const result = dayOne.partTwo(lines);
 
-        expect(result).toEqual(6)
-    })
-})
+        expect(result).toEqual(6);
+    });
+});
 
 describe('Day 1: Secret entrance - Input dataset', async () => {
-    const dataRaw = await fs.readFile(`${pathPrefix}/input.txt`)
-    const data = dataRaw.toString()
-    let lines: string[] = []
+    const dataRaw = await fs.readFile(`${pathPrefix}/input.txt`);
+    const data = dataRaw.toString();
+    let lines: string[] = [];
 
     beforeEach(async () => {
-        lines = dayOne.parse(data)
-    })
+        lines = dayOne.parse(data);
+    });
 
     it('should solve part 1', () => {
-        const result = dayOne.partOne(lines)
+        const result = dayOne.partOne(lines);
 
-        expect(result).toEqual(1147)
-    })
+        expect(result).toEqual(1147);
+    });
 
     it('should solve part 2', () => {
-        const result = dayOne.partTwo(lines)
+        const result = dayOne.partTwo(lines);
 
-        expect(result).toEqual(6789)
-    })
-})
+        expect(result).toEqual(6789);
+    });
+});
