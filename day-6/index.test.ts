385d20727012168ca2270c54205a9d215eb10bf7
vlad-georgian <144853024+vlad-georgian@users.noreply.github.com>
2026-02-19 04:52:07 +0200

    Cleanup day 6

--- day-6/index.test.ts
+++ day-6/index.test.ts
@@ -1,50 +1,52 @@
-import * as fs from 'node:fs/promises';
-import { beforeEach, describe, expect, it, test } from 'vitest';
+import * as fs from 'node:fs/promises'
+import { beforeEach, describe, expect, it } from 'vitest'
 
-import * as daySix from './index';
+import * as daySixHarness from './index.ts'
+import { partOne } from './part-one.ts'
+import { partTwo } from './part-two.ts'
 
-const pathPrefix = './day-6';
+const pathPrefix = './day-6'
 
 describe('Day 6: Trash compactor - Sample dataset', async () => {
-    const dataRaw = await fs.readFile(`${pathPrefix}/sample.txt`);
-    const data = dataRaw.toString();
-    let parts: string[] = [];
+    const dataRaw = await fs.readFile(`datasets/${pathPrefix}/sample.txt`)
+    const data = dataRaw.toString()
+    let parts: string[] = []
 
     beforeEach(async () => {
-        parts = daySix.parse(data);
-    });
+        parts = daySixHarness.parse(data)
+    })
 
     it('should solve part 1', () => {
-        const result = daySix.partOne(parts);
+        const result = partOne(parts)
 
-        expect(result).toEqual(4277556);
-    });
+        expect(result).toEqual(4277556)
+    })
 
     it('should solve part 2', () => {
-        const result = daySix.partTwo(parts);
+        const result = partTwo(parts)
 
-        expect(result).toEqual(3263827);
-    });
-});
+        expect(result).toEqual(3263827)
+    })
+})
 
 describe('Day 6: Trash compactor - Input dataset', async () => {
-    const dataRaw = await fs.readFile(`${pathPrefix}/input.txt`);
-    const data = dataRaw.toString();
-    let parts: string[] = [];
+    const dataRaw = await fs.readFile(`datasets/${pathPrefix}/input.txt`)
+    const data = dataRaw.toString()
+    let parts: string[] = []
 
     beforeEach(async () => {
-        parts = daySix.parse(data);
-    });
+        parts = daySixHarness.parse(data)
+    })
 
     it('should solve part 1', () => {
-        const result = daySix.partOne(parts);
+        const result = partOne(parts)
 
-        expect(result).toEqual(6503327062445);
-    });
+        expect(result).toEqual(6503327062445)
+    })
 
     it('should solve part 2', () => {
-        const result = daySix.partTwo(parts);
+        const result = partTwo(parts)
 
-        expect(result).toEqual(9640641878593);
-    });
-});
+        expect(result).toEqual(9640641878593)
+    })
+})
