9691964d91e26f1a88e68f17b79d5ec82cfd094d
vlad-georgian <144853024+vlad-georgian@users.noreply.github.com>
2026-02-19 04:53:54 +0200

    Cleanup day 8
    - added separate directories for each approach since there's also shared
    logic between parts now
    - upgraded the code to be TS strict

--- day-8/index.test.ts
+++ day-8/index.test.ts
@@ -1,66 +1,69 @@
 import * as fs from 'node:fs/promises'
-import { beforeEach, describe, expect, it, test } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 
-import * as dayEightBruteOptimized from './brute-force-optimized.js'
-import * as dayEightBrute from './brute-force.js'
-import * as dayEightDisjointSet from './disjoint-set.js'
-import * as dayEight from './index.js'
+import { partOne as partOneBruteOptimized } from './brute-force-optimized/part-one.ts'
+import { partTwo as partTwoBruteOptimized } from './brute-force-optimized/part-two.ts'
+import { partOne as partOneBrute } from './brute-force/part-one.ts'
+import { partTwo as partTwoBrute } from './brute-force/part-two.ts'
+import { partOne as partOneDisjointSet } from './disjoint-set/part-one.ts'
+import { partTwo as partTwoDisjointSet } from './disjoint-set/part-two.ts'
+import * as dayEightHarness from './index.ts'
 
 const pathPrefix = './day-8'
 
 describe('Day 8: Playground - Sample dataset', async () => {
-    const dataRaw = await fs.readFile(`${pathPrefix}/sample.txt`)
+    const dataRaw = await fs.readFile(`datasets/${pathPrefix}/sample.txt`)
     const data = dataRaw.toString()
     let lines: string[] = []
 
     beforeEach(async () => {
-        lines = dayEight.parse(data)
+        lines = dayEightHarness.parse(data)
     })
 
     it('should solve part 1 using the brute force approach', () => {
-        const result = dayEightBrute.partOne(lines, 10, 3)
+        const result = partOneBrute(lines, 10, 3)
 
         expect(result).toEqual(40)
     })
 
     it('should solve part 1 using the optimized brute force approach', () => {
-        const result = dayEightBruteOptimized.partOne(lines, 10, 3)
+        const result = partOneBruteOptimized(lines, 10, 3)
 
         expect(result).toEqual(40)
     })
 
     it('should solve part 1 using the disjoint set approach', () => {
-        const result = dayEightDisjointSet.partOne(lines, 10, 3)
+        const result = partOneDisjointSet(lines, 10, 3)
 
         expect(result).toEqual(40)
     })
 
     it('should solve part 2 using the brute force approach', () => {
-        const result = dayEightBrute.partTwo(lines)
+        const result = partTwoBrute(lines)
 
         expect(result).toEqual(25272)
     })
 
     it('should solve part 2 using the optimized brute force approach', () => {
-        const result = dayEightBruteOptimized.partTwo(lines)
+        const result = partTwoBruteOptimized(lines)
 
         expect(result).toEqual(25272)
     })
 
     it('should solve part 2 using the disjoint set approach', () => {
-        const result = dayEightDisjointSet.partTwo(lines)
+        const result = partTwoDisjointSet(lines)
 
         expect(result).toEqual(25272)
     })
 })
 
 describe('Day 8: Playground - Input dataset', async () => {
-    const dataRaw = await fs.readFile(`${pathPrefix}/input.txt`)
+    const dataRaw = await fs.readFile(`datasets/${pathPrefix}/input.txt`)
     const data = dataRaw.toString()
     let lines: string[] = []
 
     beforeEach(async () => {
-        lines = dayEight.parse(data)
+        lines = dayEightHarness.parse(data)
     })
 
     it('should solve part 1 using the brute force approach', () => {
--- day-8/index.ts
+++ day-8/index.ts
@@ -1,22 +1,34 @@
-import { partOne as partOneBruteOptimized, partTwo as partTwoBruteOptimized } from './brute-force-optimized.js'
-import { partOne as partOneBrute, partTwo as partTwoBrute } from './brute-force.js'
-import { partOne as partOneDisjointSet, partTwo as partTwoDisjointSet } from './disjoint-set.js'
+import { partOne as partOneBruteOptimized } from './brute-force-optimized/part-one.ts'
+import { partTwo as partTwoBruteOptimized } from './brute-force-optimized/part-two.ts'
+import { partOne as partOneBrute } from './brute-force/part-one.ts'
+import { partTwo as partTwoBrute } from './brute-force/part-two.ts'
+import { partOne as partOneDisjointSet } from './disjoint-set/part-one.ts'
+import { partTwo as partTwoDisjointSet } from './disjoint-set/part-two.ts'
 
-function parse(input: string) {
+export function parse(input: string) {
     return input.toString().trimEnd().split('\r\n')
 }
 
-function* solve(input: string, dataset?: 'sample' | 'input'): Generator<(string | number)[]> {
+export function* solve(input: string, optimal?: boolean, dataset?: 'sample' | 'input'): Generator<(string | number)[]> {
     const data = parse(input)
     const connectionLimit = dataset === 'sample' ? 10 : 1000
 
-    yield ['Day 8: Playground - Part 1 (Brute force)', 'Result', partOneBrute(data, connectionLimit, 3)]
-    yield ['Day 8: Playground - Part 1 (Brute force - Optimized)', 'Result', partOneBruteOptimized(data, connectionLimit, 3)]
-    yield ['Day 8: Playground - Part 1 (Disjoint set)', 'Result', partOneDisjointSet(data, connectionLimit, 3)]
+    // Part 1 solutions. Optimal solution is usually the "brute force optimized" approach
+    if (!optimal) {
+        yield ['Day 8: Playground - Part 1 (Brute force)', 'Result', partOneBrute(data, connectionLimit, 3)]
+    }
 
-    yield ['Day 8: Playground - Part 2 (Brute force)', 'Result', partTwoBrute(data)]
-    yield ['Day 8: Playground - Part 2 (Brute force - Optimized)', 'Result', partTwoBruteOptimized(data)]
+    yield ['Day 8: Playground - Part 1 (Brute force - Optimized)', 'Result', partOneBruteOptimized(data, connectionLimit, 3)]
+
+    if (!optimal) {
+        yield ['Day 8: Playground - Part 1 (Disjoint set)', 'Result', partOneDisjointSet(data, connectionLimit, 3)]
+    }
+
+    // Part 2 solutions. Optimal solution is usually the "disjoint set" approach
+    if (!optimal) {
+        yield ['Day 8: Playground - Part 2 (Brute force)', 'Result', partTwoBrute(data)]
+        yield ['Day 8: Playground - Part 2 (Brute force - Optimized)', 'Result', partTwoBruteOptimized(data)]
+    }
+
     return ['Day 8: Playground - Part 2 (Disjoint set)', 'Result', partTwoDisjointSet(data)]
 }
-
-export { parse, solve }
