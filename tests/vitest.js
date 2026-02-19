c4e73f0604651f5175de70f0abbfa2147cfaa7e1
vlad-georgian <144853024+vlad-georgian@users.noreply.github.com>
2026-02-19 04:48:08 +0200

    Updated the oxlint configs

--- tests/vitest.js
+++ tests/vitest.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { execaSync } from 'execa';
 
-execaSync('node', ['./node_modules/vitest/bin/vitest.js', 'run', ...process.argv.slice(2)], {
+execaSync('node', ['./node_modules/vitest/bin/vitest.ts', 'run', ...process.argv.slice(2)], {
     stdio: 'inherit',
 });
