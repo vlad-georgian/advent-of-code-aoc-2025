67cfdf363476307269d183d7ffd1f5e853fe2d2d
vlad-georgian <144853024+vlad-georgian@users.noreply.github.com>
2026-02-19 04:51:51 +0200

    Cleanup day 5

--- day-5/index.test.ts
+++ day-5/index.test.ts
@@ -1,50 +1,52 @@
-import * as fs from 'node:fs/promises';
-import { beforeEach, describe, expect, it, test } from 'vitest';
+import * as fs from 'node:fs/promises'
+import { beforeEach, describe, expect, it } from 'vitest'
 
-import * as dayFive from './index';
+import * as dayFiveHarness from './index.ts'
+import { partOne } from './part-one.ts'
+import { partTwo } from './part-two.ts'
 
-const pathPrefix = './day-5';
+const pathPrefix = './day-5'
 
 describe('Day 5: Cafeteria - Sample dataset', async () => {
-    const dataRaw = await fs.readFile(`${pathPrefix}/sample.txt`);
-    const data = dataRaw.toString();
-    let parts: string[] = [];
+    const dataRaw = await fs.readFile(`datasets/${pathPrefix}/sample.txt`)
+    const data = dataRaw.toString()
+    let parts: string[] = []
 
     beforeEach(async () => {
-        parts = dayFive.parse(data);
-    });
+        parts = dayFiveHarness.parse(data)
+    })
 
     it('should solve part 1', () => {
-        const result = dayFive.partOne(parts);
+        const result = partOne(parts)
 
-        expect(result).toEqual(3);
-    });
+        expect(result).toEqual(3)
+    })
 
     it('should solve part 2', () => {
-        const result = dayFive.partTwo(parts);
+        const result = partTwo(parts)
 
-        expect(result).toEqual(14);
-    });
-});
+        expect(result).toEqual(14)
+    })
+})
 
 describe('Day 5: Cafeteria - Input dataset', async () => {
-    const dataRaw = await fs.readFile(`${pathPrefix}/input.txt`);
-    const data = dataRaw.toString();
-    let parts: string[] = [];
+    const dataRaw = await fs.readFile(`datasets/${pathPrefix}/input.txt`)
+    const data = dataRaw.toString()
+    let parts: string[] = []
 
     beforeEach(async () => {
-        parts = dayFive.parse(data);
-    });
+        parts = dayFiveHarness.parse(data)
+    })
 
     it('should solve part 1', () => {
-        const result = dayFive.partOne(parts);
+        const result = partOne(parts)
 
-        expect(result).toEqual(896);
-    });
+        expect(result).toEqual(896)
+    })
 
     it('should solve part 2', () => {
-        const result = dayFive.partTwo(parts);
+        const result = partTwo(parts)
 
-        expect(result).toEqual(346240317247002);
-    });
-});
+        expect(result).toEqual(346240317247002)
+    })
+})
--- day-5/index.ts
+++ day-5/index.ts
@@ -1,130 +1,13 @@
-// Method: Brute force check all ranges for an ingredient. O(n*m)
-function partOne(data: string[]) {
-    if (!data || data.length < 2) {
-        throw new Error('Invalid input data provided!');
-    }
+import { partOne } from './part-one.ts'
+import { partTwo } from './part-two.ts'
 
-    // Parsing stage
-    const freshRanges = parseRanges(data[0]);
-    const ingredients = parseIngredients(data[1]);
-
-    if (!ingredients?.length || !freshRanges?.length) {
-        return 0;
-    }
-
-    let freshCount = 0;
-
-    for (const ingredient of ingredients) {
-        let fresh = false;
-
-        for (const freshRange of freshRanges) {
-            if (ingredient < freshRange[0] || ingredient > freshRange[1]) {
-                continue;
-            }
-
-            fresh = true;
-            break;
-        }
-
-        if (!fresh) {
-            continue;
-        }
-
-        freshCount++;
-    }
-
-    return freshCount;
+export function parse(input: string) {
+    return input.toString().trimEnd().split('\r\n\r\n')
 }
 
-// Method: Go through each range and check for overlaps with previous ranges. O(n^2)
-function partTwo(data: string[]): number {
-    if (!data || data.length < 2) {
-        throw new Error('Invalid input data provided!');
-    }
+export function* solve(input: string): Generator<(string | number)[]> {
+    const data = parse(input)
 
-    // Parsing stage
-    const freshRanges = parseRanges(data[0]);
-
-    if (!freshRanges?.length) {
-        return 0;
-    }
-
-    let freshCount = 0;
-
-    // Sorting fixes the special case where a previous range in the list can bisect the current one
-    freshRanges.sort((firstRange, secondRange) => firstRange[0] - secondRange[0]);
-
-    for (let rangeIndex = 0; rangeIndex < freshRanges.length; rangeIndex++) {
-        let start = freshRanges[rangeIndex][0];
-        let end = freshRanges[rangeIndex][1];
-
-        // console.log(`Evaluating range: ${start}-${end}`);
-
-        let invalidRange = false;
-
-        for (let candidateIndex = 0; candidateIndex < rangeIndex; candidateIndex++) {
-            let startCandidate = freshRanges[candidateIndex][0];
-            let endCandidate = freshRanges[candidateIndex][1];
-
-            // Full overlap with a previous range - Everything gets discarded
-            if (start >= startCandidate && end <= endCandidate) {
-                // console.log(`Discarding ${start}-${end} because it's already covered by ${startCandidate}-${endCandidate}`);
-
-                invalidRange = true;
-                break;
-            }
-
-            // Partial overlap between start of current range and end of a previous range - Lower end gets discarded
-            if (start >= startCandidate && start <= endCandidate) {
-                // const previousStart = start
-
-                start = endCandidate + 1;
-
-                // console.log(`Found overlap between ${previousStart}-${end} and ${startCandidate}-${endCandidate}. Ajusted the new range to be ${start}-${end}`);
-
-                if (end < start) {
-                    console.log(`Discarding ${start}-${end} because the range is now invalid`);
-                    invalidRange = true;
-                    break;
-                }
-            }
-        }
-
-        if (invalidRange) {
-            continue;
-        }
-
-        freshCount += end - start + 1;
-    }
-
-    return freshCount;
+    yield ['Day 5: Cafeteria - Part 1', 'Max fresh', partOne(data)]
+    return ['Day 5: Cafeteria - Part 2', 'Max fresh', partTwo(data)]
 }
-
-function parseRanges(rangesRaw: string) {
-    if (!rangesRaw) {
-        return [];
-    }
-
-    // Creates an array of tuples shaped like [ [start,end], ...]
-    return rangesRaw.split('\r\n').map((range: string): [number, number] => {
-        let parts = range.split('-');
-        return [Number(parts[0]), Number(parts[1])];
-    });
-}
-
-function parseIngredients(ingredientsRaw: string) {
-    return ingredientsRaw.split('\r\n').map(Number);
-}
-
-function parse(input: string) {
-    return input.toString().trimEnd().split('\r\n\r\n');
-}
-
-function* solve(input: string): Generator<(string | number)[]> {
-    const data = parse(input);
-
-    yield ['Day 5: Cafeteria - Part 1', 'Max fresh', partOne(data)];
-    return ['Day 5: Cafeteria - Part 2', 'Max fresh', partTwo(data)];
-}
-
-export { partOne, partTwo, parse, solve };
